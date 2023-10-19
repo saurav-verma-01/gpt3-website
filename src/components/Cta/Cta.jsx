8e1e1b18cc145be0e9fb3ec4d87378ff6d898575
Saurav kumar verma <sauravkumarverma22@gmail.com>
2023-10-19 20:56:13 +0530

    files and folder structure for the entire webpage

---
+++ src/components/Cta/Cta.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./cta.module.css";
+
+const Cta = () => {
+  return <div>Cta</div>;
+};
+
+export default Cta;
---
+++ src/components/Feature/Feature.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./feature.module.css";
+
+const Feature = () => {
+  return <div>Feature</div>;
+};
+
+export default Feature;
