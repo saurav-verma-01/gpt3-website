659aed79229600ddcc9a3e66fe62619db9264121
Saurav kumar verma <sauravkumarverma22@gmail.com>
2023-10-19 23:58:01 +0530

    working on navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
 import React from "react";
+import {
+  Blog,
+  Features,
+  Footer,
+  Header,
+  Possibility,
+  WhatGPT3,
+} from "./containers";
 
+import { Cta, NavBar, Brand } from "./components";
+
+import styles from "./app.module.css";
 const App = () => {
   return (
-    <div>
-      <h1>GPT 3</h1>
+    <div className="app">
+      <div className={styles.gradient__bg}>
+        <NavBar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <Cta />
+      <Blog />
+      <Footer />
     </div>
   );
 };
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import styles from "./navbar.module.css";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+import logo from "../../assets/logo.svg";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return <nav className{styles.navbar}>
+    <div className{styles.navbarLinks}>
+      <div className{styles.logo}>
+        <img src={logo} alt="logo" />
+      </div>
+    </div>
+  </nav>
 };
 
 export default NavBar;
