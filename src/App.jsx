3067d71735d40e1fff3d58f801a3d7175f99e9ea
Saurav kumar verma <sauravkumarverma22@gmail.com>
2023-10-20 21:00:56 +0530

    Working on Header

--- src/App.jsx
+++ src/App.jsx
@@ -10,13 +10,13 @@ import {
 
 import { Cta, NavBar, Brand } from "./components";
 
-import styles from "./app.module.css";
+import "./app.css";
 const App = () => {
   return (
-    <div className={styles.app}>
+    <div className="app">
       <div className="gradient__bg">
         <NavBar />
-        {/* <Header /> */}
+        <Header />
       </div>
       {/* <Brand />
       <WhatGPT3 />
--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -1,8 +1,35 @@
 import React from "react";
 import styles from "./header.module.css";
 
+import people from "../../assets/people.png";
+import ai from "../../assets/ai.png";
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className={`section__padding ${styles.header}`}>
+      <div className={styles.header__content}>
+        <h1 className="gradient__text">
+          Let’s Build Something amazing with GPT-3 OpenAI
+        </h1>
+        <p>
+          Yet bed any for travelling assistance indulgence unpleasing. Not
+          thoughts all exercise blessing. Indulgence way everything joy
+          alteration boisterous the attachment. Party we years to order allow
+          asked of.
+        </p>
+        <div className={styles.header__form}>
+          <input type="email" placeholder="Your Email Address" />
+          <button type="submit">Get Started</button>
+        </div>
+        <div className={styles.header__reviews}>
+          <img src={people} alt="people" />
+          <p>1,600 people requested access a visit in last 24 hours</p>
+        </div>
+      </div>
+      <div className={styles.header__illustrations}>
+        <img src={ai} alt="aiillustration" />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
